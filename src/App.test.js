e10fc35bb2625f3ff221915fa59413539b58ef02
Larry Luna <lawrence.matthew.luna@gmail.com>
2026-05-31 15:11:19 -0500

    Update React test for OVERWATCH dashboard

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders OVERWATCH dashboard', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const title = screen.getByText(/OVERWATCH/i);
+
+  expect(title).toBeInTheDocument();
 });
